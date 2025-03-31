071d969ff2c92229b053a99e211e866bbb5d6e53
Kypataka <evepegas2@gmail.com>
2025-03-31 22:39:07 +0300

    Module 1, task 3

---
+++ js-info.js
@@ -0,0 +1,9 @@
+alert('Язык программирования JavaScript разработал Брэндан Эйх (Айк) в компании Netscape');
+
+alert('JavaScript был разработан за 10 дней');
+
+alert('JavaScript был разработан в мае 1995 года');
+
+alert('JavaScript — это не Java');
+
+alert('Спецификация JavaScript называется ECMAScript');
